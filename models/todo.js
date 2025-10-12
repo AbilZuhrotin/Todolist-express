df57a1751f2b7ec584e97afe0bfa443157a3c691
Abil Zuhrotin <abilzuhrotins@gmail.com>
2025-10-12 20:14:38 +0700

    Membuat folder controllers, membuat model todo & user

---
+++ models/todo.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        id_todo: 1,
+        title: "Belajar Express JS",
+        description: "Mempelajari dasar-dasar Express JS",
+        date: "2024-06-01",
+        id_user: 1
+    }
+]
---
+++ models/users.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        id_user: 1,
+        username: "johndoe",
+        fullname: "John Doe",
+        email: "johndoe@gmail.com",
+        password: "password123"
+    }
+]
