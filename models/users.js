b568e21fddb27d0c8250e5e0d7a136f25835cbcb
Abil Zuhrotin <abilzuhrotins@gmail.com>
2025-10-13 23:33:12 +0700

    Membuat schema users

--- models/users.js
+++ models/users.js
@@ -1,9 +1,13 @@
-module.exports = [
-    {
-        id_user: 1,
-        username: "johndoe",
-        fullname: "John Doe",
-        email: "johndoe@gmail.com",
-        password: "password123"
-    }
-]
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  fullname: { type: String, required: true }, 
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
