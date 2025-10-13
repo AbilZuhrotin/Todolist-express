a8e8e72ba754014f6d0d871c1fbe6de4828f5a27
Abil Zuhrotin <abilzuhrotins@gmail.com>
2025-10-13 13:35:46 +0700

    Menambahkan controllers get todo by id

--- controllers/todo.controllers.js
+++ controllers/todo.controllers.js
@@ -8,7 +8,21 @@ module.exports = {
       data: todoModel,
     });
   },
-  getTodoById: (req, res) => {},
+
+  getTodoById: (req, res) => {
+    const id = req.params.id
+    const todo = todoModel.find((item) => item.id_todo === Number(id))
+    if (!todo){
+      return res.status(404).json({
+        massage: "Todo tidak ditemukan",
+      })
+    }
+    res.json({
+      massage: "Todo ditemukan",
+      data:todo,
+    })
+  },
+  
   createTodo: (req, res) => {},
   updateTodo: (req, res) => {},
   deleteAllTodo: (req, res) => {},
--- models/todo.js
+++ models/todo.js
@@ -5,5 +5,12 @@ module.exports = [
         description: "Mempelajari dasar-dasar Express JS",
         date: "2024-06-01",
         id_user: 1
+    },
+    {
+        id_todo: 2,
+        title: "Belajar Express JS 2",
+        description: "Mempelajari berbagai fitur Express JS",
+        date: "2024-06-01",
+        id_user: 1
     }
 ]
