0c99d6538ac40701269c1ac030b34cda319a85d6
Abil Zuhrotin <abilzuhrotins@gmail.com>
2025-10-16 11:31:57 +0700

    Menginstall bycriptjs dan menambahkan hash password

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const usersModel = require('../models/users');
 
 module.exports = {
@@ -15,18 +16,24 @@ module.exports = {
             });
         }
     },
-    
+
     login: async (req, res) => {
         try {
             const {email, password} = req.body;
-            const user = await usersModel.findOne({ email: email, password: password });
+            const user = await usersModel.findOne({ email });
             if (!user) {
                 return res.status(404).json({
                     message: "User tidak ditemukan",
                 });
             }
+            const isPasswordValid = bcrypt.compareSync(password, user.password);
+            if (!isPasswordValid) {
+                return res.status(401).json({
+                    message: "Password salah",
+                });
+            }
             res.json({
-                message: "User ditemukan",
+                message: "Login berhasil",
                 data: user,
             });
     } catch (error) {
@@ -40,11 +47,22 @@ module.exports = {
     register: (req, res) => {
         try {
             const { username, fullname, email, password } = req.body;
+
+            const duplikatEmail = usersModel.findOne({ email });
+            if (duplikatEmail) {
+                return res.status(400).json({
+                    message: "Email sudah terdaftar",
+                });
+            }
+
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+
             const newUser = new usersModel ({
                 username,
                 fullname,
                 email,
-                password
+                password: hash
         });
         newUser.save();
         res.status(201).json({
